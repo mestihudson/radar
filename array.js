06422963fb186acf1b081547b7970ba39fe000a3
mestihudson <mestihudson@gmail.com>
2016-02-26 14:31:16 -0300

    novo

---
+++ array.js
@@ -0,0 +1,14 @@
+// Array.prototype.each = function(callback){
+//   callback = typeof(callback) === 'function' ? callback : function(){};
+//   for(var i = 0, l = this.length; i < l; i++) {
+//     callback(this[i], i);
+//   }
+// };
+
+// Array.prototype.in = function(item){
+//   var result = false;
+//   this.each(function(e, i){
+//     result = (e === item);
+//   });
+//   return result;
+// };
